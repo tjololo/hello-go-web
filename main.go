ee1c4f28d373838a9b589c20b2ae86ce9f58bbe5
tjololo <vemundg@gmail.com>
2018-07-31 22:25:40 +0200

    simple web server returning a respone

---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+func main() {
+	var port int
+	flag.IntVar(&port, "web-port", 8080, "Port the webserver should launch with")
+	flag.Parse()
+	log.Printf("Staring application on port %d...\n", port)
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Hello from go code")
+	})
+	server := http.Server{Addr: fmt.Sprintf(":%d", port)}
+	go func() {
+		log.Fatal(server.ListenAndServe())
+	}()
+
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, syscall.SIGINT, syscall.SIGTERM)
+	<-signalChan
+
+	log.Println("Shutdown received, exiting...")
+
+	server.Shutdown(context.Background())
+}
