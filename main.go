69dab0a131d39a4124b386b5f96044110bbffc18
tjololo <vemundg@gmail.com>
2018-08-01 00:38:19 +0200

    response can be tweaked with -response flag. To ease testing of twitters diffy

--- main.go
+++ main.go
@@ -13,12 +13,23 @@ import (
 
 func main() {
 	var port int
+	var response string
 	flag.IntVar(&port, "port", 8080, "Port the webserver should launch with")
+	flag.StringVar(&response, "response", "Hello from go code", "Content of respone element")
 	flag.Parse()
 	log.Printf("Staring application on port %d...\n", port)
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, "Hello from go code")
+	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
+		log.Println("Got a request")
+		log.Println(fmt.Sprintf("{\"respons\":\"%s\"}\n", response))
+		w.Header().Set("Content-Type", "application/json; charset=utf-8")
+		w.WriteHeader(200)
+		w.Write([]byte(fmt.Sprintf("{\"respons\":\"%s\"}\n", response)))
 	})
+
+	http.HandleFunc("/favicon.ico", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "favicon.ico")
+	})
+
 	server := http.Server{Addr: fmt.Sprintf(":%d", port)}
 	go func() {
 		log.Fatal(server.ListenAndServe())
